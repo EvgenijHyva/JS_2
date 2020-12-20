7d63aefa8c01e281c15c05000feec32f353b2913
Evgeny <evgeny.hyvarinen@gmail.com>
2020-12-20 23:58:33 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const products = [
+    { id: 1, title: 'Notebook', price: 2000 },
+    { id: 2, title: 'Mouse', price: 20 },
+    { id: 3, title: 'Keyboard', price: 200 },
+    { id: 4, title: 'Gamepad', price: 50 },
+];
+//Функция для формирования верстки каждого товара
+const renderProduct = (obj) => {
+    return `<div class="product-item">
+                <img>
+                <h3>${obj.title}</h3>
+                <p>${obj.price}</p>
+                <button id="${obj.id}" class="buy-btn">Купить</button>
+            </div>`
+};
+const renderPage = list => {
+    const productsList = list.map(item => renderProduct(item));
+    console.log(productsList);
+    document.querySelector('.products').innerHTML = productsList;
+};
+
+renderPage(products);
